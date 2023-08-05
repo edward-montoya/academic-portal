364e9fc89657d268bada4ae1456a304ed075def0
Edward Montoya <emontoy@rei.com>
2023-08-05 06:37:35 -0500

    Initial structure - contentstack application

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <header>
+            <p>Header</p>
+        </header>
+        <main>
+            <slot />
+        </main>
+        <footer>
+            <p>Footer</p>
+        </footer>
+    </div>
+</template>
